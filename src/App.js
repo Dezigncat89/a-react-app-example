c3ca980030d7cbf7ec93589504f54e7af1bff6c6
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-05-11 08:39:58 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,6 +34,18 @@ class App extends Component {
   <ExampleComponent />
 </div>
 
+class TestComponent extends Component {
+  render() {
+    return (
+      <div className="video">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/5TbUxGZtwGI" frameBorder="0" allow="autoplay; encrypted-media" title="time video" allowFullScreen></iframe>
+      </div>
+    );
+  }
+}
+
+export default TestComponent;
+
 
 
 
